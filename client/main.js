5551605927d7d41900cb7d6e7cf11e87ce82f03f
dkuku <daniel.kuku@gmail.com>
2018-07-04 03:14:10 +0100

    integrated selectedNoteId into notes list and url part 2

--- client/main.js
+++ client/main.js
@@ -3,7 +3,10 @@ import { Meteor } from 'meteor/meteor';
 import ReactDOM from 'react-dom';
 import { Tracker } from 'meteor/tracker';
 import { Session } from 'meteor/session';
+import { createBrowserHistory } from 'history'
 import 'babel-polyfill';
+
+const history = createBrowserHistory();
 
 import { routes, onAuthChange } from '../imports/routes/routes';
 import '../imports/startup/simple-schema-configuration.js';
@@ -12,7 +15,14 @@ Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
   onAuthChange(isAuthenticated);
 });
+Tracker.autorun(() => {
+    const selectedNoteId = Session.get('selectedNoteId');
+    if (selectedNoteId) {
+        history.replace(`/dashboard/${selectedNoteId}`);
+    }
+});
 
 Meteor.startup(() => {
+    Session.set('selectedNoteId', undefined);
     ReactDOM.render(routes, document.getElementById('app'));
 });
